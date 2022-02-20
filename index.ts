47423fa5337db50f51a51fdda70529f527d97fbd
leecr1215 <leecr1215@naver.com>
2022-02-20 23:56:14 +0900

    finish #0.4 First steps with Typescript

--- index.ts
+++ index.ts
@@ -1,1 +1,10 @@
-console.log("hello");
+const name = "Nicolas",
+  age = 24,
+  gender = "male";
+
+// ?를 붙여서 파라미터인 gender가 선택적이라는 것을 의미해줌
+const sayHi = (name, age, gender?) => {
+  console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+};
+sayHi(name, age);
+export {};
